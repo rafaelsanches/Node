67e5c7ab6ef41a4dfddea20b8377e3b760018e9d
rafael garcia <rafael@rafaels-MacBook-Pro.local>
2017-10-20 13:50:26 -0200

    port configuration

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.set('view engine','hbs');
@@ -43,4 +45,4 @@ app.get('/about',(req,res)=> {
         res.render('about.hbs',dinamicData);
     });
 
-app.listen(3000);
+app.listen(port);
